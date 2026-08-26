65fc87b9af964eb5ab73127082252a472502cc1d
Raihan A. <266201844+raihan-rifat007@users.noreply.github.com>
2026-08-26 10:22:00 +0600

    Add files via upload

--- scripts/update-deps.js
+++ scripts/update-deps.js
@@ -3,7 +3,7 @@ const path = require("path");
 const https = require("https");
 const { execSync } = require("child_process");
 
-const DEPENDENCIES = ["@distube/ytdl-core", "fs-extra"];
+const DEPENDENCIES = ["axios", "fs-extra"];
 
 function fetchLatestVersion(pkg) {
   return new Promise((resolve, reject) => {
